6fa14d8ef0ad764daa2b13b091515189241dc488
alfredlau <1009273187@qq.com>
2021-12-02 16:44:31 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import NotesList from "./components/NotesList";
+const App = () => {
+  const [notes, setNotes] = useState([
+    { id: nanoid(), text: "Hi! This is Alfred!", date: "2021/11/20" },
+    { id: nanoid(), text: "Hi! This is Simon!", date: "2021/11/21" },
+  ]);
 
-function App() {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("alfred-note-app-data"));
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("alfred-note-app-data", JSON.stringify(notes));
+  }, [notes]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNote = notes.filter((note) => note.id !== id);
+    setNotes(newNote);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search handleSearchNote={setSearchText} />
+      <NotesList
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText.toLowerCase())
+        )}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
-}
+};
 
 export default App;
